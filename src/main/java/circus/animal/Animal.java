81a7c8bc68bb69a32a25f029e9fdec682735a9f2
chewyang <owenzhouyang98@gmail.com>
2020-09-04 21:29:31 +0800

    lecture stuff

---
+++ src/main/java/circus/animal/Animal.java
@@ -0,0 +1,8 @@
+package circus.animal;
+
+import circus.Asset;
+
+public abstract class Animal implements Asset {
+    
+    public abstract String speak(); //method body deleted this is saying all objects should have this speak
+}
